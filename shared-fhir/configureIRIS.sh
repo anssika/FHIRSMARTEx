ec1e7080a4136fede5d88b3141e016c42f513872
anssikauppi <64200350+anssikauppi@users.noreply.github.com>
2024-01-23 16:08:39 +0200

    did fixes

--- shared-fhir/configureIRIS.sh
+++ shared-fhir/configureIRIS.sh
@@ -4,12 +4,14 @@ iris session $ISC_PACKAGE_INSTANCENAME -U %SYS <<- END
 
 Zn "%SYS"
 Do ##class(%SYSTEM.OBJ).ImportDir("/src/FHIRSMARTEx/Setup", "*.cls", "c" , .errlog, 1, .imported)
+Do ##class(%SYSTEM.OBJ).ImportDir("/src/FHIRSMARTEx/OAuth2", "*.cls", "c" , .errlog, 1, .imported)
 Do ##class(FHIRSMARTEx.Setup.OAuth2Server).Setup()
 Zn "FHIR"
 Do ##class(%SYSTEM.OBJ).ImportDir("/src", "*.cls", "c" , .errlog, 1, .imported)
 Do ##class(FHIRSMARTEx.Setup.OAuth2Clients).Setup()
 Do ##class(FHIRSMARTEx.Setup.FHIRServer).Setup()
 Do ##class(FHIRSMARTEx.Setup.Interop).Setup()
+Do ##class(FHIRSMARTEx.Setup.FHIRResources).Setup()
 
 Halt
 END
